9348ff9ebaa73a92395b81dbf8a846fd6dc7f8c7
josephagnelo <josephagnelo@users.noreply.github.com>
2024-01-23 14:00:25 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,27 @@ module.exports = {
  
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          // fixes proxy-agent dependencies
+          net: false,
+          dns: false,
+          tls: false,
+          assert: false,
+          // fixes next-i18next dependencies
+          path: false,
+          fs: false,
+          // fixes mapbox dependencies
+          events: false,
+          // fixes sentry dependencies
+          process: false
+        }
+      };
     }
+    config.module.exprContextCritical = false; // Workaround to suppress next-i18next warning, see https://github.com/isaachinman/next-i18next/issues/1545
 
-    return config
+    return config;
   },
   
 }
